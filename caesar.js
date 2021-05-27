d4364a562a9c9f17c2351a374e1c7ec206b42f03
gmgrieb <gmgrieb@uno.edu>
2021-05-26 19:54:08 -0500

    fixed bookmark storage and other fixes

--- caesar.js
+++ caesar.js
@@ -2,18 +2,14 @@ console.log("hello");
 var amount=1;
 var iframe;
 
-//const search_iframe=document.getElementById("PT_AGSTARTPAGE_NUI");
-//const search_iframe=document.getElementById("main_target_win0");
-//console.log(search_iframe);
 
 function onFrameLoaded(){
     iframe=document.getElementById("main_target_win0");
-    //console.log(document.getElementById("main_target_win0"));
+    //checking if the iframe contain an element with "CLASS_SEARCH", this tells us if were on the class search page(and not for example on a the shopping cart page)
     if(iframe.contentWindow.document.getElementById("CLASS_SEARCH")){
         
-        console.log("HEYY");
-        //iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0").selectedIndex=2;
-       // iframe.contentWindow.document.getElementById("win0div$ICField232$0").innerHTML+=`<p id="save_button">Save</p><p id="load_button">Load</p>`;
+       // console.log("HEYY");
+        //adds buttons and selector
         iframe.contentWindow.document.getElementById("win0div$ICField232$0").innerHTML+=`</br><div style="width:600px;"><div style="display: flex; justify-content: center;"><input type="button" id="save_button" tabindex="995" value="Save" class="PSPUSHBUTTON" style="width:50px;"> 
                                                                                         <input type="button" id="load_button" tabindex="995" value="Load" class="PSPUSHBUTTON" style="width:50px;">
                                                                                         <select class="PSDROPDOWNLIST" id="bookmarks" style="width:220px; "></select>
@@ -21,9 +17,11 @@ function onFrameLoaded(){
         iframe.contentWindow.document.getElementById("save_button").onclick=saveButton;
         iframe.contentWindow.document.getElementById("load_button").onclick=loadButton;
         iframe.contentWindow.document.getElementById("remove_button").onclick=removeButton;
-        chrome.storage.sync.onChanged.addListener(onItemChanged);
+        //when the bookmark storage is changed call onItemChanged()
+        chrome.storage.local.onChanged.addListener(onItemChanged);
+        //call onItemChanged to set up the initial selector list
         onItemChanged();
-        
+        //when the iframe is changed call oniFrameChange
         const frameobserver = new MutationObserver(oniFrameChange);
         frameobserver.observe(iframe.contentWindow.document, observerOptions);
         
@@ -31,30 +29,31 @@ function onFrameLoaded(){
 
 
 }
+//checks if iframe exists and if it does set a listener to call onFrameLoaded() when the iframe does load
 function onParentLoaded(ev){
-    //if(ev[0])
-    //console.log(ev);
-    //console.log(ev[0].addedNodes[0]);
-    //console.log(typeof ev[0].addedNodes[0]);
-    //console.log(document.getElementById("DERIVED_REGFRM1_SS_TRANSACT_TITLE"));
-    
-    
     if(document.getElementById("main_target_win0")){
         document.getElementById("main_target_win0").onload=onFrameLoaded;
         
     }
    
 }
+//called on iframe change, used to add the bookmark elements back when the page is reloaded(happens when the user tries to do an invalid search or clicks the clear button)
 function oniFrameChange(){
     
     console.log('iframe changed');
-    if(!iframe.contentWindow.document.getElementById("save_button")&&iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT").textContent&&iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility=="hidden"){
-        onFrameLoaded();
-        console.log(iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility);
-        console.log('reload');
-    } 
-    else if(!iframe.contentWindow.document.getElementById("save_button")&&!iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT").textContent&&iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility=="hidden"){
 
+    //this is for when the user does an invalid search
+    if(!iframe.contentWindow.document.getElementById("save_button")&&iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT")&&iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT").textContent&&iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility=="hidden"){
+        onFrameLoaded();
+        console.log('reload because of search error');
+    } 
+    //this is when the user clicks the clears button
+    else if(!iframe.contentWindow.document.getElementById("save_button")&&
+            (!iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT")||(iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT")&&!iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT").textContent))
+            &&iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility=="hidden"){
+
+                onFrameLoaded();
+                console.log('reload because of clear');
     }
 }
 function saveButton(){
@@ -64,7 +63,7 @@ function saveButton(){
     console.log('amount'+amount);
     
    
-    chrome.storage.sync.set({[amount]: [iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0").selectedIndex,
+    chrome.storage.local.set({[amount]: [iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0").selectedIndex,
     iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$1").selectedIndex,
     iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_CATALOG_NBR$1").value,
     iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_ACAD_CAREER$2").selectedIndex,
@@ -94,13 +93,13 @@ function removeButton(){
     
     const remove_ind=iframe.contentWindow.document.getElementById("bookmarks").selectedIndex+1;
     console.log('remove '+remove_ind)
-    chrome.storage.sync.remove(remove_ind.toString());
+    chrome.storage.local.remove(remove_ind.toString());
 }
 function onItemChanged(){
     console.log("changed");
     
-    chrome.storage.sync.get(null, function(result) {
-        console.log(result);
+    chrome.storage.local.get(null, function(result) {
+        //console.log(result);
         var index=1;
         iframe.contentWindow.document.getElementById("bookmarks").innerHTML=``;
         for (const [key, value] of Object.entries(result)) {
@@ -120,21 +119,18 @@ function onItemChanged(){
             }
             //const match= iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$1").options[value[1]].text;
             const number=value[2];
-            iframe.contentWindow.document.getElementById("bookmarks").innerHTML+=`<option value="${value}">${subject} that ${match} ${number}</option>`;
+            iframe.contentWindow.document.getElementById("bookmarks").innerHTML+=`<option value="${value}">${subject} ${match} ${number}</option>`;
             index=index+1;
         };
     });
 }
-//window.addEventListener('load',onSearchLoaded);
+//setting up initial listener that sees when the iframe parent is changed
 const iframe_parent=document.getElementById("win0divPSPAGECONTAINER_MD");
 var observerOptions = {
     childList: true,
-    //attributes: true,
-  
-    // Omit (or set to false) to observe only changes to the parent node
+
     subtree: true
   }
+  //call onParentLoaded if parent element is changed
 const observer = new MutationObserver(onParentLoaded);
 observer.observe(iframe_parent, observerOptions);
-chrome.storage.sync.clear();
-//chrome.webRequest.onHeadersReceived.addListener(onSearchLoaded);
