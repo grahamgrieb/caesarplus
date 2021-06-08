a1056eb0d4fe2cd784bca230d856a499df172429
gmgrieb <gmgrieb@uno.edu>
2021-06-08 16:18:17 -0500

    commented out console.log's, removed test items, increased version to 1.2

--- caesar.js
+++ caesar.js
@@ -1,14 +1,14 @@
-console.log("hello");
+//console.log("hello");
 //chrome.storage.local.clear();
 var amount;
 var iframe;
 var changing_term = false;
 var opening_advanced=false;
+var serif_on=true;
 var current_value;
 
 class SearchBookmark {
     constructor(subjectId, subjectName, courseSelector, courseNumber, career, open, keyword, term, startSelector, startNumber, endSelector, endNumber, days, mon, tues, wed, thurs, fri,sat,sun, instructNameSelector, instructName, classNbr, campus, courseComp, session) {
-        console.log("leng of args "+arguments.length)
         this.subjectId = subjectId;
         this.subjectName = subjectName;
         this.courseSelector = courseSelector;
@@ -59,13 +59,12 @@ const observer = new MutationObserver(onParentLoaded);
 observer.observe(iframe_parent, observerOptions);
 
 function onFrameLoaded() {
-    //console.log('iframe loaded');
+    ////console.log('iframe loaded');
     iframe = document.getElementById("main_target_win0");
     //checking if the iframe contain an element with "CLASS_SEARCH", this tells us if were on the class search page(and not for example on a the shopping cart page)
     const frameobserver = new MutationObserver(oniFrameChange);
     if (iframe.contentWindow.document.getElementById("pt_pageinfo_win0").getAttribute('page') == "SSR_CLSRCH_ENTRY") {
-        //<input type="button" id="load_button" tabindex="995" value="Load" class="PSPUSHBUTTON" style="width:50px;" data-termchanged="0" onclick="while(this.dataset.termchanged!="1"); addchg_win0(this); submitAction_win0(this.form,this.id); this.dataset.termchanged="2";>
-        console.log("search page");
+       //console.log("search page");
         //adds buttons and selector
         iframe.contentWindow.document.getElementById("win0div$ICField143").outerHTML = `</br><div style="width:600px;"><div style="display: flex; justify-content: center;"><label class="PSDROPDOWNLABEL">Bookmarks </label><input type="button" id="save_button" tabindex="995" value="Save" class="PSPUSHBUTTON" style="width:50px;"> 
                                                                                     <input type="button" id="load_button" tabindex="995" value="Load" class="PSPUSHBUTTON" style="width:50px;">
@@ -75,7 +74,7 @@ function onFrameLoaded() {
         iframe.contentWindow.document.getElementById("save_button").onclick = saveButton;
         iframe.contentWindow.document.getElementById("load_button").addEventListener("click", loadButton);
         iframe.contentWindow.document.getElementById("remove_button").onclick = removeButton;
-
+       
 
 
 
@@ -106,13 +105,13 @@ function onParentLoaded(ev) {
 //called on iframe change, used to add the bookmark elements back when the page is reloaded(happens when the user tries to do an invalid search or clicks the clear button)
 function oniFrameChange() {
 
-    console.log('iframe changed');
+    //console.log('iframe changed');
     //if on search page
     if (iframe.contentWindow.document.getElementById("pt_pageinfo_win0").getAttribute('page') == "SSR_CLSRCH_ENTRY") {
         //this is for when the user does an invalid search
         if (!iframe.contentWindow.document.getElementById("save_button") && iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT") && iframe.contentWindow.document.getElementById("win0divDERIVED_CLSMSG_ERROR_TEXT").textContent && iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility == "hidden") {
             onFrameLoaded();
-            console.log('reload because of search error');
+            //console.log('reload because of search error');
         }
         //this is when the user clicks the clears button
         else if (!iframe.contentWindow.document.getElementById("save_button") &&
@@ -120,7 +119,7 @@ function oniFrameChange() {
             && iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility == "hidden") {
 
             onFrameLoaded();
-            console.log('reload because of clear');
+            //console.log('reload because of clear');
             //if it was changing the term because of a bookmark load
             if (changing_term) {
                 loadButtonAfter();
@@ -136,8 +135,8 @@ function oniFrameChange() {
     }
     //if on results page
     else if (iframe.contentWindow.document.getElementById("pt_pageinfo_win0").getAttribute('page') == "SSR_CLSRCH_RSLT" && iframe.contentWindow.document.getElementById("WAIT_win0").style.visibility == "hidden") {
-        if (!iframe.contentWindow.document.getElementById("tab_button")) {
-            console.log("results page")
+        if (!iframe.contentWindow.document.getElementById("tabbutton1")) {
+            //console.log("results page")
 
             resultsPage()
         }
@@ -146,13 +145,13 @@ function oniFrameChange() {
 
 
 function saveButton() {
-    console.log("saving");
+    //console.log("saving");
 
 
-    console.log('amount' + amount);
+    //console.log('amount' + amount);
     var bookmark;
     if (iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_START_TIME_OPR$5")) {
-        console.log("advanced");
+        //console.log("advanced");
         bookmark = new SearchBookmark(iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0").options[iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0").selectedIndex].text, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$1").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_CATALOG_NBR$1").value, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_ACAD_CAREER$2").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_OPEN_ONLY$3").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_DESCR$4").value, iframe.contentWindow.document.getElementById("CLASS_SRCH_WRK2_STRM$35$").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_START_TIME_OPR$5").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_MEETING_TIME_START$5").value, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_END_TIME_OPR$5").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_MEETING_TIME_END$5").value, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_INCLUDE_CLASS_DAYS$6").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_MON$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_TUES$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_WED$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_THURS$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_FRI$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SAT$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SUN$6").checked, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_EXACT_MATCH2$7").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_LAST_NAME$7").value, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_CLASS_NBR$8").value, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_CAMPUS$9").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_COMPONENT$10").selectedIndex, iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SESSION_CODE$11").selectedIndex)
     }
     else {
@@ -162,8 +161,8 @@ function saveButton() {
         [amount]: bookmark
     },
         function (result) {
-            console.log("bookmark ");
-            console.log(bookmark);
+            //console.log("bookmark ");
+            //console.log(bookmark);
 
         });
     amount = amount + 1;
@@ -171,8 +170,8 @@ function saveButton() {
 function loadButton(){
     const ind =iframe.contentWindow.document.getElementById("bookmarks").options[iframe.contentWindow.document.getElementById("bookmarks").selectedIndex].value;
     chrome.storage.local.get(ind, function (result) {
-        console.log("loading ")
-        console.log(result)
+        //console.log("loading ")
+        //console.log(result)
         current_value=result[ind];
         loadButtonAfter();
     })
@@ -202,7 +201,7 @@ function loadButtonAfter(ind) {
             }
             //needs advanced pane but its already expanded
             else if(current_value.advancedSearch&&iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_START_TIME_OPR$5")){
-                console.log("advanced pane already open");
+                //console.log("advanced pane already open");
                 iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_START_TIME_OPR$5").selectedIndex=current_value.startSelector;
                 iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_MEETING_TIME_START$5").value=current_value.startNumber;
                 iframe.contentWindow.document.getElementById("SSR_CLSRCH_WRK_SSR_END_TIME_OPR$5").selectedIndex=current_value.endSelector;
@@ -236,16 +235,16 @@ function loadButtonAfter(ind) {
 function removeButton() {
 
     const remove_ind = iframe.contentWindow.document.getElementById("bookmarks").options[iframe.contentWindow.document.getElementById("bookmarks").selectedIndex].value;
-    console.log('remove ' + remove_ind);
+    //console.log('remove ' + remove_ind);
     chrome.storage.local.remove(remove_ind);
 }
 
 
 function onItemChanged() {
-    console.log("changed");
+    //console.log("changed");
 
     chrome.storage.local.get(null, function (result) {
-        console.log(result);
+        //console.log(result);
         
         
         var index = 1;
@@ -285,8 +284,8 @@ function onItemChanged() {
 
 
 function resultsPage() {
-    iframe.contentWindow.document.getElementById("win0divDERIVED_CLSRCH_GROUP5").innerHTML += `<a id="tab_button"  style="width:50px;" ptlinktgt="pt_peoplecode" " class="PSHYPERLINK">Load</a>`
-    iframe.contentWindow.document.getElementById("tab_button").onclick = function () { newTab(0) };
+    //iframe.contentWindow.document.getElementById("win0divDERIVED_CLSRCH_GROUP5").innerHTML += `<a id="tab_button"  style="width:50px;" ptlinktgt="pt_peoplecode" " class="PSHYPERLINK">Load</a>`
+    //iframe.contentWindow.document.getElementById("tab_button").onclick = function () { newTab(0) };
     var ind = 0;
     iframe.contentWindow.document.querySelectorAll('*[id^="ACE_SSR_CLSRSLT_WRK_GROUPBOX2$"]').forEach(elem => {
         elem.outerHTML += `<input type="button" id="morebutton${ind}" tabindex="995" value="More Info" class="PSPUSHBUTTON" style="width:60px;"></input>    <input type="button" id="tabbutton${ind}" tabindex="995" value="Open in New Tab" class="PSPUSHBUTTON" style="width:100px;"></input>`;
@@ -307,7 +306,7 @@ function resultsPage() {
 
 }
 function newTab(ind) {
-    console.log(ind);
+    //console.log(ind);
     var xhr = new XMLHttpRequest();
 
     var fd = `ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum=${iframe.contentWindow.document.getElementById("ICStateNum").value}&ICAction=MTG_CLASSNAME%24${ind}&ICModelCancel=0&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICSkipPending=0&ICAutoSave=0&ICResubmit=0&ICSID=${iframe.contentWindow.document.getElementById("ICSID").value}&ICAGTarget=true&ICActionPrompt=false&ICBcDomData=UnknownValue&ICPanelHelpUrl=&ICPanelName=&ICFind=&ICAddCount=&ICAppClsData=&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$27$=9999`;
@@ -317,7 +316,7 @@ function newTab(ind) {
 
         const newTab = iframe.contentWindow.document.cloneNode(true);
 
-        console.log(newTab);
+        //console.log(newTab);
         newTab.getElementById("win0divPSPAGECONTAINER").outerHTML = event.target.responseXML.getElementById("win0divPAGECONTAINER").innerHTML.slice(9, -3)
         newTab.getElementById("ACE_$ICField241").outerHTML="";
         newTab.getElementById("win0divNW_DERIVED_SS_NW_LINK_CRSE_DESCR").parentElement.parentElement.outerHTML="";
@@ -335,7 +334,7 @@ function newTab(ind) {
     xhr.send(fd);
 }
 function moreInfo(ind) {
-    console.log(ind);
+    //console.log(ind);
     var xhr = new XMLHttpRequest();
 
     var fd = `ICAJAX=1&ICNAVTYPEDROPDOWN=0&ICType=Panel&ICElementNum=0&ICStateNum=${iframe.contentWindow.document.getElementById("ICStateNum").value}&ICAction=MTG_CLASSNAME%24${ind}&ICModelCancel=0&ICXPos=0&ICYPos=0&ResponsetoDiffFrame=-1&TargetFrameName=None&FacetPath=None&ICFocus=&ICSaveWarningFilter=0&ICChanged=-1&ICSkipPending=0&ICAutoSave=0&ICResubmit=0&ICSID=${iframe.contentWindow.document.getElementById("ICSID").value}&ICAGTarget=true&ICActionPrompt=false&ICBcDomData=UnknownValue&ICPanelHelpUrl=&ICPanelName=&ICFind=&ICAddCount=&ICAppClsData=&DERIVED_SSTSNAV_SSTS_MAIN_GOTO$27$=9999`;
@@ -345,7 +344,7 @@ function moreInfo(ind) {
 
         const newTab = iframe.contentWindow.document.cloneNode(true);
 
-        console.log(newTab);
+        //console.log(newTab);
         newTab.getElementById("win0divPSPAGECONTAINER").outerHTML = event.target.responseXML.getElementById("win0divPAGECONTAINER").innerHTML.slice(9, -3)
         if (newTab.getElementById("SSR_CLS_DTL_WRK_SSR_REQUISITE_LONG")) {
             iframe.contentWindow.document.getElementById(`extra_info${ind}`).innerHTML += `<br>Class Capacity: ${newTab.getElementById("SSR_CLS_DTL_WRK_ENRL_TOT").innerText}/${newTab.getElementById("SSR_CLS_DTL_WRK_ENRL_CAP").innerText}<br><br>Wait List Total: ${newTab.getElementById("SSR_CLS_DTL_WRK_WAIT_TOT").innerText} <br><br>Reqs: ${newTab.getElementById("SSR_CLS_DTL_WRK_SSR_REQUISITE_LONG").innerText}`;
